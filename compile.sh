d4e620cd206c56fe08ca5ea75d441881200f18f2
Michael Neumann <mneumann@ntecs.de>
2014-07-22 13:31:48 +0200

    Compile libcore

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@ sh install.sh
 mkdir -p target
 
 rustc --target x86_64-pc-freebsd-elf --crate-type lib src/my.rs -o target/libmy.rlib
+rustc --target x86_64-pc-freebsd-elf --crate-type lib ./rust-nightly/src/libcore/lib.rs -o target/libcore.rlib
 rustc --target x86_64-pc-freebsd-elf --emit obj -L target src/main.rs -o target/main.o
-
 
 clang \
  -L./df-tree/usr/lib \
@@ -12,4 +12,4 @@ clang \
  -B./df-tree/usr/lib \
  -B./df-tree/usr/lib/gcc47 \
  -target x86_64-unknown-dragonfly \
- -o target/app target/main.o ./rust-nightly/src/rt/arch/x86_64/morestack.S target/libmy.rlib
+ -o target/app target/main.o ./rust-nightly/src/rt/arch/x86_64/morestack.S target/libmy.rlib target/libcore.rlib
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 #![feature(lang_items)]
 
 extern crate my;
+extern crate core;
 
 #[no_mangle]
 pub extern fn main(_argc: int, _argv: *const *const u8) -> int {
@@ -11,3 +12,6 @@ pub extern fn main(_argc: int, _argv: *const *const u8) -> int {
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "begin_unwind"] extern fn rust_begin_unwind(a: &core::fmt::Arguments, b: &str, c: uint) {
+  // XXX: never return
+}
