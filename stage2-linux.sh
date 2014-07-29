7407d3ac8b2dc7e3c35bd2893525cc93a07c46af
Michael Neumann <mneumann@ntecs.de>
2014-07-29 02:28:08 +0200

    Stage2: jemalloc

--- stage2-linux.sh
+++ stage2-linux.sh
@@ -23,9 +23,9 @@ fi
 TOP=`pwd`
 
 RUST_PREFIX=${TOP}/stage1-linux/install
-RUST_SRC=${TOP}/stage1-linux/rust
+RUST_SRC=${TOP}/stage2-linux/rust
 RUSTC=${RUST_PREFIX}/bin/rustc
-TARGET=x86_64-pc-dragonfly-elf
+TARGET=x86_64-unknown-dragonfly
 
 DF_LIB_DIR=${TOP}/stage1-dragonfly/libs
 RS_LIB_DIR=${TOP}/stage2-linux/rust-libs
@@ -35,6 +35,16 @@ export LD_LIBRARY_PATH=${RUST_PREFIX}/lib
 mkdir -p ${TOP}/stage2-linux
 mkdir -p ${TOP}/stage2-linux/rust-libs
 
+if [ ! -e ${TOP}/stage2-linux/rust ]; then
+  cd stage2-linux
+  git clone --reference ${TOP}/stage1-linux/rust https://github.com/mneumann/rust.git
+  cd rust
+  git checkout dragonfly
+  cd ${TOP}
+fi
+
+cp ${TOP}/stage1-dragonfly/llvmdeps.rs ${TOP}/stage2-linux/rust/src/librustc_llvm/
+
 # XXX
 export CFG_VERSION="0.12.0-pre-nightly"
 export CFG_RELEASE="dragonfly-cross"
@@ -42,6 +52,8 @@ export CFG_VER_HASH="hash"
 export CFG_VER_DATE="`date`"
 export CFG_COMPILER_HOST_TRIPLE="x86_64-unknown-dragonfly"
 export CFG_PREFIX="/usr/local"
+
+RUST_FLAGS="--cfg jemalloc"
 
 RUST_LIBS="core libc alloc unicode collections rustrt rand sync std native arena rustuv debug log fmt_macros serialize term syntax flate time url uuid getopts regex test coretest glob graphviz num rustc_back semver rustc_llvm rustc fourcc hexfloat regex_macros green rustdoc"
 
@@ -51,12 +63,10 @@ for lib in $RUST_LIBS; do
     echo "skipping $lib"
   else
     echo "compiling $lib"
-    ${RUSTC} --target ${TARGET} --crate-type lib -L${DF_LIB_DIR} -L${RS_LIB_DIR} ${RUST_SRC}/src/lib${lib}/lib.rs -o ${RS_LIB_DIR}/lib${lib}.rlib
+    ${RUSTC} --target ${TARGET} ${RUST_FLAGS} --crate-type lib -L${DF_LIB_DIR} -L${DF_LIB_DIR}/llvm -L${RS_LIB_DIR} ${RUST_SRC}/src/lib${lib}/lib.rs -o ${RS_LIB_DIR}/lib${lib}.rlib
   fi
 done
 
-LLVM_LIBS="-lLLVMLTO -lLLVMObjCARCOpts -lLLVMLinker -lLLVMipo -lLLVMVectorize -lLLVMBitWriter -lLLVMIRReader -lLLVMAsmParser -lLLVMR600CodeGen -lLLVMR600Desc -lLLVMR600Info -lLLVMR600AsmPrinter -lLLVMSystemZDisassembler -lLLVMSystemZCodeGen -lLLVMSystemZAsmParser -lLLVMSystemZDesc -lLLVMSystemZInfo -lLLVMSystemZAsmPrinter -lLLVMHexagonCodeGen -lLLVMHexagonAsmPrinter -lLLVMHexagonDesc -lLLVMHexagonInfo -lLLVMNVPTXCodeGen -lLLVMNVPTXDesc -lLLVMNVPTXInfo -lLLVMNVPTXAsmPrinter -lLLVMCppBackendCodeGen -lLLVMCppBackendInfo -lLLVMMSP430CodeGen -lLLVMMSP430Desc -lLLVMMSP430Info -lLLVMMSP430AsmPrinter -lLLVMXCoreDisassembler -lLLVMXCoreCodeGen -lLLVMXCoreDesc -lLLVMXCoreInfo -lLLVMXCoreAsmPrinter -lLLVMMipsDisassembler -lLLVMMipsCodeGen -lLLVMMipsAsmParser -lLLVMMipsDesc -lLLVMMipsInfo -lLLVMMipsAsmPrinter -lLLVMAArch64Disassembler -lLLVMAArch64CodeGen -lLLVMAArch64AsmParser -lLLVMAArch64Desc -lLLVMAArch64Info -lLLVMAArch64AsmPrinter -lLLVMAArch64Utils -lLLVMARMDisassembler -lLLVMARMCodeGen -lLLVMARMAsmParser -lLLVMARMDesc -lLLVMARMInfo -lLLVMARMAsmPrinter -lLLVMPowerPCDisassembler -lLLVMPowerPCCodeGen -lLLVMPowerPCAsmParser -lLLVMPowerPCDesc -lLLVMPowerPCInfo -lLLVMPowerPCAsmPrinter -lLLVMSparcDisassembler -lLLVMSparcCodeGen -lLLVMSparcAsmParser -lLLVMSparcDesc -lLLVMSparcInfo -lLLVMSparcAsmPrinter -lLLVMTableGen -lLLVMDebugInfo -lLLVMOption -lLLVMX86Disassembler -lLLVMX86AsmParser -lLLVMX86CodeGen -lLLVMSelectionDAG -lLLVMAsmPrinter -lLLVMX86Desc -lLLVMMCDisassembler -lLLVMX86Info -lLLVMX86AsmPrinter -lLLVMX86Utils -lLLVMJIT -lLLVMLineEditor -lLLVMMCAnalysis -lLLVMInstrumentation -lLLVMInterpreter -lLLVMCodeGen -lLLVMScalarOpts -lLLVMInstCombine -lLLVMTransformUtils -lLLVMipa -lLLVMAnalysis -lLLVMProfileData -lLLVMMCJIT -lLLVMTarget -lLLVMRuntimeDyld -lLLVMObject -lLLVMMCParser -lLLVMBitReader -lLLVMExecutionEngine -lLLVMMC -lLLVMCore -lLLVMSupport"
+${RUSTC} ${RUST_FLAGS} --emit obj -o ${TOP}/stage2-linux/driver.o --target ${TARGET} -L${DF_LIB_DIR} -L${RS_LIB_DIR} --cfg rustc ${RUST_SRC}/src/driver/driver.rs
 
-LINK="-target ${TARGET} -B${DF_LIB_DIR}/usr/lib/gcc47 -B${DF_LIB_DIR}/usr/lib -B${DF_LIB_DIR}/lib -L${DF_LIB_DIR}/lib -L${DF_LIB_DIR}/usr/lib -L${DF_LIB_DIR}/usr/lib/gcc47 -L${DF_LIB_DIR}/lib -L${DF_LIB_DIR}/usr/lib/gcc47 -L${DF_LIB_DIR} -rpath .${DF_LIB_DIR}/usr/lib/gcc47 -rpath ${DF_LIB_DIR}/usr/lib -rpath ${DF_LIB_DIR}/lib -Wl,-whole-archive ${LLVM_LIBS} -Wl,-no-whole-archive -lz -lpthread -ledit -ltinfo -lm -lc -lrt -lstdc++ -lcompiler-rt -lrt"
-
-${RUSTC} --emit obj -C linker=clang -C link-args="${LINK}" --target ${TARGET} -L${DF_LIB_DIR} -L${RS_LIB_DIR} --cfg rustc ${RUST_SRC}/src/driver/driver.rs
+tar cvzf ${TOP}/stage2-linux.tgz stage2-linux/*.o stage2-linux/rust-libs
