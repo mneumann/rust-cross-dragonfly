9a7ff80bf4a28776255d0709afffc18b3aeca88d
Michael Neumann <mneumann@ntecs.de>
2014-12-14 21:01:53 +0100

    jit component does no longer exist

--- stage1-dragonfly.sh
+++ stage1-dragonfly.sh
@@ -121,7 +121,7 @@ cp -r /usr/lib ${TARGET}/usr/lib
 
 # 
 cd ${TOP}/..
-python ${TOP}/rust/src/etc/mklldeps.py stage1-dragonfly/llvmdeps.rs "x86 arm mips ipo bitreader bitwriter linker asmparser jit mcjit interpreter instrumentation" true "${LLVM_TARGET}/bin/llvm-config"
+python ${TOP}/rust/src/etc/mklldeps.py stage1-dragonfly/llvmdeps.rs "x86 arm mips ipo bitreader bitwriter linker asmparser mcjit interpreter instrumentation" true "${LLVM_TARGET}/bin/llvm-config"
 
 cd ${TOP}/..
 tar cvzf stage1-dragonfly.tgz stage1-dragonfly/${TARGET_SUB} stage1-dragonfly/llvmdeps.rs
