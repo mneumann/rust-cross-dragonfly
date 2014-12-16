674f3e9afc0ab892826f6613320187f90795064c
Michael Neumann <mneumann@ntecs.de>
2014-12-16 12:38:32 +0100

    Make easier parameterizable

--- stage4-dragonfly.sh
+++ stage4-dragonfly.sh
@@ -3,18 +3,21 @@ if [ `uname -s` != "DragonFly" ]; then
   exit 1
 fi
 
-if [ ! -e "stage3-dragonfly/bin/rustc" ]; then
-  echo "stage3-dragonfly does not exist!"
-  exit 1
-fi
-mkdir -p stage4-dragonfly
-
 TOP=`pwd`
+LOCAL_RUST_ROOT=${TOP}/stage3-dragonfly
 PREFIX=/usr/local
 BRANCH=dragonfly4
+DST_DIR=${TOP}/stage4-dragonfly
 
-if [ ! -e ${TOP}/stage4-dragonfly/rust ]; then
-  cd stage4-dragonfly
+if [ ! -e "${LOCAL_RUST_ROOT}/bin/rustc" ]; then
+  echo "Local rust compiler does not exist!"
+  exit 1
+fi
+
+mkdir -p ${DST_DIR}
+
+if [ ! -e ${DST_DIR}/rust ]; then
+  cd ${DST_DIR}
   git clone --depth 1 --branch ${BRANCH} https://github.com/mneumann/rust.git
   cd rust
   git submodule init
@@ -22,9 +25,9 @@ if [ ! -e ${TOP}/stage4-dragonfly/rust ]; then
   cd ${TOP}
 fi
 
-cd ${TOP}/stage4-dragonfly/rust
+cd ${DST_DIR}/rust
 
-./configure --enable-local-rust --local-rust-root=${TOP}/stage3-dragonfly --prefix=$PREFIX 2>stage4.err >stage4.out
+./configure --enable-local-rust --local-rust-root=${LOCAL_RUST_ROOT} --prefix=$PREFIX 2>stage4.err >stage4.out
 cd src/llvm
 patch -p1 < ${TOP}/patch-llvm
 cd ../..
