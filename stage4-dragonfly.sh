ea98025f64f3fce10472ee6457c5113f74bd59a9
Michael Neumann <mneumann@ntecs.de>
2014-12-16 12:28:13 +0100

    Cleanup

--- stage4-dragonfly.sh
+++ stage4-dragonfly.sh
@@ -10,14 +10,12 @@ fi
 mkdir -p stage4-dragonfly
 
 TOP=`pwd`
+PREFIX=/usr/local
+BRANCH=dragonfly4
 
 if [ ! -e ${TOP}/stage4-dragonfly/rust ]; then
   cd stage4-dragonfly
-  if [ -e ${TOP}/stage1-dragonfly/rust ]; then
-    git clone --reference ${TOP}/stage1-dragonfly/rust https://github.com/rust-lang/rust.git
-  else
-    git clone https://github.com/rust-lang/rust.git
-  fi
+  git clone --depth 1 --branch ${BRANCH} https://github.com/mneumann/rust.git
   cd rust
   git submodule init
   git submodule update
@@ -25,15 +23,15 @@ if [ ! -e ${TOP}/stage4-dragonfly/rust ]; then
 fi
 
 cd ${TOP}/stage4-dragonfly/rust
-./configure --enable-local-rust --local-rust-root=${TOP}/stage3-dragonfly --prefix=/usr/local
+
+./configure --enable-local-rust --local-rust-root=${TOP}/stage3-dragonfly --prefix=$PREFIX 2>stage4.err >stage4.out
 cd src/llvm
 patch -p1 < ${TOP}/patch-llvm
-cd ../jemalloc
-patch -p1 < ${TOP}/patch-jemalloc
 cd ../..
 
-gmake
+gmake 2>>stage4.err >>stage4.out
 
 p=`pwd`
 
-echo "To install to /usr/local: cd $p && gmake install"
+gmake snap-stage3
+echo "To install to $PREFIX: cd $p && gmake install"
