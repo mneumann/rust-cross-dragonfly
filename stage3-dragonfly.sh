dfea620be192daa504d57410b59003421cae9238
Michael Neumann <mneumann@ntecs.de>
2014-12-16 12:02:41 +0100

    Use correct library destination

--- stage3-dragonfly.sh
+++ stage3-dragonfly.sh
@@ -23,20 +23,18 @@ LLVM_LIBS="-lLLVMLTO -lLLVMObjCARCOpts -lLLVMLinker -lLLVMipo -lLLVMVectorize -l
 
 RUST_DEPS="$RL/librustdoc.rlib $RL/librustc_driver.rlib $RL/libregex_macros.rlib $RL/librustc_trans.rlib $RL/librustc_typeck.rlib $RL/librustc_borrowck.rlib $RL/librustc.rlib $RL/librbml.rlib $RL/librustc_llvm.rlib $RL/librustc_back.rlib $RL/libgraphviz.rlib $RL/libcoretest.rlib $RL/libtest.rlib $RL/libgetopts.rlib $RL/libtime.rlib $RL/libflate.rlib $RL/libsyntax.rlib $RL/libterm.rlib $RL/libserialize.rlib $RL/libfmt_macros.rlib $RL/liblog.rlib $RL/libregex.rlib $RL/libarena.rlib $RL/libstd.rlib $RL/librand.rlib $RL/librustrt.rlib $RL/libcollections.rlib $RL/libunicode.rlib $RL/liballoc.rlib $RL/liblibc.rlib $RL/libcore.rlib"
 
+TARGET=x86_64-unknown-dragonfly
+DST_LIB=stage3-dragonfly/lib/rustlib/${TARGET}/lib
 
 mkdir -p stage3-dragonfly/bin
-mkdir -p stage3-dragonfly/lib
+mkdir -p ${DST_LIB}
 
 cc -m64 -o stage3-dragonfly/bin/rustc stage2-linux/driver.o ${RUST_DEPS} -L./stage1-dragonfly/libs/llvm -L./stage1-dragonfly/libs $SUP_LIBS $LLVM_LIBS -lrt -lpthread -lgcc_pic -lc -lm -lz -ledit -ltinfo -lstdc++
 
 echo "rustc done"
 
-cp stage1-dragonfly/libs/libcompiler-rt.a stage3-dragonfly/lib
-cp stage1-dragonfly/libs/libmorestack.a stage3-dragonfly/lib
-cp stage2-linux/rust-libs/*.rlib stage3-dragonfly/lib
+cp stage1-dragonfly/libs/libcompiler-rt.a ${DST_LIB}
+cp stage1-dragonfly/libs/libmorestack.a ${DST_LIB}
+cp stage2-linux/rust-libs/*.rlib ${DST_LIB}
 
-# On DragonFly self_exe_name() returns a path like 
-# "/pfs/@@-1:00004/mneumann/test" which contains a `:'.
-# Joining this path clearly fails.
-# Specifying --sysroot overcomes this problem for now. 
-./stage3-dragonfly/bin/rustc -Lstage3-dragonfly/lib --sysroot /usr hw.rs && ./hw
+#./stage3-dragonfly/bin/rustc -Lstage3-dragonfly/lib hw.rs && ./hw
